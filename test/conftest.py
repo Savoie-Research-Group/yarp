81badcd48612876918111e993e75d8046f6e2c87
Ericka Roy Miller <eer33@case.edu>
2025-05-20 11:24:16 -0400

    add unit testing suite for smiles2adjmat() function

--- test/conftest.py
+++ test/conftest.py
@@ -20,3 +20,23 @@ def ethene_xyz():
 def ethene_smi():
     """Returns the smiles string for ethene."""
     return "C=C"
+
+
+@pytest.fixture
+def haa_canon_smi():
+    return 'O=CCO'
+
+
+@pytest.fixture
+def haa_full_map_smi():
+    return '[C:0]([C:1]([O:7][H:11])([H:13])[H:14])(=[O:6])[H:12]'
+
+
+@pytest.fixture
+def haa_heavy_map_smi():
+    return '[CH:1]([CH2:4][OH:3])(=[O:2])'
+
+
+@pytest.fixture
+def haa_heavy_map_explicitH_smi():
+    return '[C:1]([C:4]([O:3][H])([H])[H])(=[O:2])[H]'
---
+++ test/yarpecule/graph/test_smiles.py
@@ -0,0 +1,307 @@
+"""
+Testing suite for functions contained in yarp/yarpecule/graph/smiles.py
+"""
+import pytest
+import yarp.yarpecule.graph.smiles as smi
+
+
+class TestSmi2Adj:
+    def test_ethene(self, ethene_smi):
+        adjmat, atom_info = smi.smiles2adjmat(ethene_smi)
+
+        elements = [label[0] for label in atom_info]
+        assert elements == ['C', 'C', 'H', 'H', 'H', 'H']
+
+        assert adjmat.shape == (6, 6)
+
+        assert adjmat[0, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 1] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[0, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[0, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[0, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 5] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[1, 0] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[1, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 4] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[1, 5] == pytest.approx(1.0, rel=1e-5)
+
+        assert adjmat[2, 0] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 5] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[3, 0] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 5] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[4, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 1] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[4, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 5] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[5, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 1] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[5, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 5] == pytest.approx(0.0, rel=1e-5)
+
+    def test_haa_canon(self, haa_canon_smi):
+        adjmat, atom_info = smi.smiles2adjmat(haa_canon_smi)
+
+        elements = [label[0] for label in atom_info]
+        assert elements == ['O', 'C', 'C', 'O', 'H', 'H', 'H', 'H']
+
+        # TO-DO: test adjmat outputs
+        assert adjmat.shape == (8, 8)
+
+        assert adjmat[0, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 1] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[0, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[1, 0] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[1, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[1, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 4] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[1, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[2, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 1] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 5] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 6] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[3, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 7] == pytest.approx(1.0, rel=1e-5)
+
+        assert adjmat[4, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 1] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[4, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[5, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[5, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[6, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[6, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[7, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[7, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 7] == pytest.approx(0.0, rel=1e-5)
+
+    def test_haa_full_map(self, haa_full_map_smi):
+        adjmat, atom_info = smi.smiles2adjmat(haa_full_map_smi)
+
+        elements = [label[0] for label in atom_info]
+        assert elements == ['C', 'C', 'O', 'O', 'H', 'H', 'H', 'H']
+
+        # TO-DO: test adjmat outputs in detail
+        assert adjmat.shape == (8, 8)
+
+    def test_haa_heavy_map(self, haa_heavy_map_smi):
+        adjmat, atom_info = smi.smiles2adjmat(haa_heavy_map_smi)
+
+        elements = [label[0] for label in atom_info]
+        assert elements == ['C', 'O', 'O', 'C', 'H', 'H', 'H', 'H']
+
+        assert adjmat.shape == (8, 8)
+
+        assert adjmat[0, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 1] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[0, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[0, 4] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[0, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[1, 0] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[1, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[2, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 7] == pytest.approx(1.0, rel=1e-5)
+
+        assert adjmat[3, 0] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 5] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 6] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[4, 0] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[4, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[5, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[5, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[6, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[6, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[7, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[7, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 7] == pytest.approx(0.0, rel=1e-5)
+
+    def test_haa_heavy_map_explicitH(self, haa_heavy_map_explicitH_smi):
+        adjmat, atom_info = smi.smiles2adjmat(haa_heavy_map_explicitH_smi)
+
+        elements = [label[0] for label in atom_info]
+        assert elements == ['C', 'O', 'O', 'C', 'H', 'H', 'H', 'H']
+
+        assert adjmat.shape == (8, 8)
+
+        assert adjmat[0, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 1] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[0, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[0, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[0, 7] == pytest.approx(1.0, rel=1e-5)
+
+        assert adjmat[1, 0] == pytest.approx(2.0, rel=1e-5)
+        assert adjmat[1, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[1, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[2, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 4] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[2, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[2, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[3, 0] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[3, 5] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 6] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[3, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[4, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 2] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[4, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[4, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[5, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[5, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[5, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[6, 0] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 3] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[6, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[6, 7] == pytest.approx(0.0, rel=1e-5)
+
+        assert adjmat[7, 0] == pytest.approx(1.0, rel=1e-5)
+        assert adjmat[7, 1] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 2] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 3] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 4] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 5] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 6] == pytest.approx(0.0, rel=1e-5)
+        assert adjmat[7, 7] == pytest.approx(0.0, rel=1e-5)

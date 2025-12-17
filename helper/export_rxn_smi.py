264e6de941467976e762153727078793e14fc495
Thomas J. Burton <tburton2@nd.edu>
2025-12-17 10:35:19 -0500

    Updated export, slight simplifications

--- helper/export_rxn_smi.py
+++ helper/export_rxn_smi.py
@@ -30,15 +30,19 @@ def main(args):
             "reactant_smi": rxn.reactant.map_smi,
             "product_smi": rxn.product.map_smi,
             }
-
+        if args.canon:
+            row["reactant_canon_smi"] = rxn.reactant.canon_smi
+            row["product_canon_smi"] = rxn.product.canon_smi
         if args.egat:
             # safely format as a string or keep numeric; here I keep numeric rounded
             egat = rxn.barrier.get("egat", None) if hasattr(rxn, "barrier") else None
             row["egat_barrier"] = None if egat is None else float(f"{egat:.5g}")
 
-        if args.cantera:
+        if args.flux:
             mf = getattr(rxn, "max_flux", None)
             row["max_flux"] = None if mf is None else float(f"{mf:.5g}")
+            
+
 
         rows.append(row)
 
@@ -46,7 +50,7 @@ def main(args):
 
     # Optional: enforce column order (nice for reproducibility)
     base_cols = ["rxn_id", "rxn_hash", "reactant_smi", "product_smi"]
-    extra_cols = (["egat_barrier"] if args.egat else []) + (["max_flux"] if args.cantera else [])
+    extra_cols = (["reactant_canon_smi","product_canon_smi"] if args.canon else []) + (["egat_barrier"] if args.egat else []) + (["max_flux"] if args.flux else [])
     df = df[base_cols + extra_cols]
 
     df.to_csv(args.output, index=False)
@@ -56,10 +60,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extract mapped SMILES from YARP pickle file")
     parser.add_argument("filename", help="Path to the pickle file")
     parser.add_argument("output", help="Path to the CSV file with SMILES strings")
+    parser.add_argument("-c", "--canon",  action="store_true",
+                        help="Include Canonical SMILES")
     parser.add_argument("-e", "--egat", action="store_true",
                         help="Include EGAT barriers for each reaction")
-    parser.add_argument("-c", "--cantera", action="store_true",
+    parser.add_argument("-f", "--flux", action="store_true",
                         help="Include Cantera max_flux values for each reaction")
+
 
     args = parser.parse_args()
     main(args)

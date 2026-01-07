bdc5af447d700962976fb2bf11c4cbecf03f1c1b
Ericka Roy Miller <emille35@nd.edu>
2026-01-07 14:57:14 -0500

    add integration test for enum2egat

--- test/main_yarp/test_main_yarp.py
+++ test/main_yarp/test_main_yarp.py
@@ -32,3 +32,20 @@ def test_enum_full(enum_full_options):
         saved_reactions = pickle.load(f)
 
     assert len(saved_reactions) == 2
+
+def test_egat_minimal(egat_min_options):
+    input_dict = egat_min_options
+    main(input_dict)
+
+    output_str = input_dict['initialize']['output']
+    output = Path(output_str)
+    assert output.exists(), "Output pickle file was not created"
+
+    with open(output_str, "rb") as f:
+        saved_reactions = pickle.load(f)
+
+    assert len(saved_reactions) == 2
+
+    rxns = list(saved_reactions.values())
+    assert rxns[0].barrier['egat'] == pytest.approx(97.13385772705078, rel=1e-5)
+    assert rxns[1].barrier['egat'] == pytest.approx(73.41398620605469, rel=1e-5)

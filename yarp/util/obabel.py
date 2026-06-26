1c31ca07db0716b98cb2aaf0df873733e20a830f
Ericka Roy Miller <eer33@case.edu>
2026-06-26 14:51:24 -0400

    more robust prod geom generation after enumeration - attempt to use rdkit, then fall back to (more reliable) open babel - add checks for preserved connectivity

---
+++ yarp/util/obabel.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+from openbabel import pybel
+
+from yarp.util.write_files import mol_write_yp
+
+def obabel_ff_opt(molecule, lot="uff", maxiter=500):
+    '''
+    Perform low-level level geometry optimization on yarpecule using openbabel.
+
+    Parameters:
+    ----------
+    molecule : yarpecule object
+        molecule to be optimized 
+
+    lot : string
+        Level of theory used for quick optimization
+
+    Returns
+    -------
+    opt_geom : nd array (N x 3)
+        optimized geometry
+    '''
+
+    # Write yarpecule object to a temporary mol file
+    mol_file = '.tmp.mol'
+    mol_write_yp(mol_file, molecule.elements, molecule.geo,
+                 molecule.bond_mats[0], molecule.adj_mat)
+
+    # Use openbabel to perform geometry optimization
+    mol = next(pybel.readfile("mol", mol_file))
+    mol.localopt(forcefield=lot, steps=maxiter)
+
+    # Delete temporary mol file
+    os.system("rm {}".format(mol_file))
+
+    # Collect optimized geometry coordinates
+    opt_geom = np.zeros_like(molecule.geo)
+    for count_i, i in enumerate(opt_geom):
+        opt_geom[count_i] = mol.atoms[count_i].coords
+
+    return opt_geom

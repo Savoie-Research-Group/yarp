bd47d08f6ac6aa04c9b071ed0de8be9ea5312f76
Thomas J. Burton <tburton2@nd.edu>
2026-07-10 12:45:46 -0400

    Keep conformer scoring in lightweight model container

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     name="yarp",  # This is the name you'll use to import or pip install
     version="3.0.0",
     packages=find_packages(),  # Automatically finds yarp/ and its subpackages
-    package_data={
-        "yarp.reaction.conf_sampling": ["*.sav"],
-    },
     install_requires=[],  # Add dependencies here if needed
     entry_points={  # Allows for easy command-line executables
         'console_scripts': [

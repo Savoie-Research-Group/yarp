6c5956d608e37c1d78e28672b3fa77692be27550
Ericka Roy Miller <139382043+erm42@users.noreply.github.com>
2026-07-22 18:06:59 -0400

    update version labels (#45)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2025, Savoie Research Group'
 author = 'Savoie Research Group'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '3.0.1'
 #release = version('yarp_again')
 # for example take major/minor
 version = release
